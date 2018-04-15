9f7c83717b1117ad715e72e05a83cd25272060d9
Unknown <companys.albert@gmail.com>
2018-04-15 17:55:13 +0200

    Preprocessing lib and testing notebook

--- preprocessing.py
+++ preprocessing.py
@@ -22,21 +22,24 @@ def is_valid_position(position, shape):
 def calculate_boundaries(mask, pos, patch_size):
     width, height = mask.shape[0], mask.shape[1]
     
-    x_min, x_max, y_min, y_max = -1, -1, -1, -1
+    x_min = pos[0] - patch_size//2
+    x_max = pos[0] + patch_size//2 - 1
+    y_min = pos[1] - patch_size//2
+    y_max = pos[1] + patch_size//2 - 1
     
-    if pos[0] < patch_size//2 - 1:
+    if x_min < 0:
         x_min = 0
-        x_max = x_min + patch_size
-    elif pos[0] > width - patch_size//2 + 1:
+        x_max = x_min + patch_size - 1
+    elif x_max >= width:
         x_max = width - 1
-        x_min = x_max - patch_size
+        x_min = x_max - patch_size + 1
 
-    if pos[1] < patch_size//2 - 1:
+    if y_min < 0:
         y_min = 0
-        y_max = y_min + patch_size
-    elif pos[1] > height - patch_size//2 + 1:
+        y_max = y_min + patch_size - 1
+    elif y_max >= height:
         y_max = height - 1
-        y_min = y_max - patch_size
+        y_min = y_max - patch_size + 1
 
     return x_min, x_max, y_min, y_max
 
@@ -44,11 +47,10 @@ def calculate_boundaries(mask, pos, patch_size):
 def get_patch(mask, image, patch_size=32, containing_road=True):
 
     pos = tuple(np.random.choice(mask.shape[0], 2))
-    print(pos)
     value = mask[pos]
 
     if containing_road:
-        while value==0 and (not is_valid_position(pos, mask.shape)):
+        while value==0 or (not is_valid_position(pos, mask.shape)):
             pos = tuple(np.random.choice(mask.shape[0], 2))
             value = mask[pos]
         x_min, x_max, y_min, y_max = calculate_boundaries(mask, pos, patch_size)
@@ -57,3 +59,43 @@ def get_patch(mask, image, patch_size=32, containing_road=True):
     else:
         x_min, x_max, y_min, y_max = calculate_boundaries(mask, pos, patch_size)
         return mask[x_min:x_max, y_min:y_max], image[x_min:x_max, y_min:y_max, :]
+
+if __name__ == "__main__":
+    print("Testing preprocessing.py")
+    assert is_valid_position((-1, 1), (250, 250)) == False
+    assert is_valid_position((251, 2), (250, 250)) == False 
+    assert is_valid_position((2, 300), (250, 250)) == False 
+    assert is_valid_position((2, -1), (250, 250)) == False 
+    assert is_valid_position((2, 2), (250, 250)) == True 
+
+    print("1/2 tests run successfully")
+
+    M = np.zeros((10,10))
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (2,2), 4)
+    assert (x_min == 0 and x_max == 3 and y_min == 0 and y_max == 3)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (5,2), 4)
+    assert (x_min == 3 and x_max == 6 and y_min == 0 and y_max == 3)
+    
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (8,2), 4)
+    assert (x_min == 6 and x_max == 9 and y_min == 0 and y_max == 3)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (8,5), 4)
+    assert (x_min == 6 and x_max == 9 and y_min == 3 and y_max == 6)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (2,9), 4)
+    assert (x_min == 0 and x_max == 3 and y_min == 6 and y_max == 9)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (2,5), 4)
+    assert (x_min == 0 and x_max == 3 and y_min == 3 and y_max == 6)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (8,8), 4)
+    assert (x_min == 6 and x_max == 9 and y_min == 6 and y_max == 9)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (5,8), 4)
+    assert (x_min == 3 and x_max == 6 and y_min == 6 and y_max == 9)
+
+    x_min, x_max, y_min, y_max = calculate_boundaries(M, (5,5), 4)
+    assert (x_min == 3 and x_max == 6 and y_min == 3 and y_max == 6)
+
+    print("2/2 tests run successfully")
